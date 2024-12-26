228e8b5dc043d237ef7016ae50e1fe1206223afa
eva <1916291530@qq.com>
2024-12-26 17:56:53 +0800

    springmvc

---
+++ 22/springMVC/springMVC-demo01/src/main/java/com/atguigu/mvc/Main.java
@@ -0,0 +1,12 @@
+package com.atguigu.mvc;
+
+/**
+ * @Description:
+ * @Author Eva
+ * @Date 2024/12/26 15:12
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
